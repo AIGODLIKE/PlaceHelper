4d55f0e6cbee1656fddfdcb7674fe607e71b2fc4
atticus <1029910278@qq.com>
2023-01-14 15:10:41 +0800

    fix:placetool 多物体gizmo朝向修复

--- place_tool/gzg.py
+++ place_tool/gzg.py
@@ -58,6 +58,10 @@ class PH_GZG_place_tool(GZGBase, bpy.types.GizmoGroup):
                 z = get_objs_bbox_top(
                     [obj for obj in context.selected_objects if obj.type == 'MESH'])
 
+                # 统一gizmo朝上
+                self.rotate_gz.matrix_basis = Matrix()
+                self.scale_gz.matrix_basis = Matrix()
+
                 self.rotate_gz.matrix_basis.translation = Vector((top.x, top.y, z))
                 self.scale_gz.matrix_basis.translation = Vector((top.x, top.y, z))
 
@@ -72,193 +76,9 @@ class PH_GZG_place_tool(GZGBase, bpy.types.GizmoGroup):
             self.correct_gz_loc(context)
 
 
-class TEST_GGT_test_group2(GZGBase, bpy.types.GizmoGroup):
-    bl_label = "Test Widget"
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'WINDOW'
-    # 'TOOL_INIT' also sounds appropriate, but then the gizmo doesn't appear!
-    bl_options = {'3D'}
-
-    _move_gz = {}
-
-    @classmethod
-    def poll(cls, context):
-        return context.object and len(context.selected_objects) > 0
-
-    def setup(self, context):
-        self._move_gz.clear()
-        self.add_move_gz(context, 'X')
-        self.add_move_gz(context, 'Y')
-        self.add_move_gz(context, 'Z')
-
-    def add_move_gz(self, context, axis):
-        ui = bpy.context.preferences.themes[0].user_interface
-
-        axis_x = ui.axis_x[:3]
-        axis_y = ui.axis_y[:3]
-        axis_z = ui.axis_z[:3]
-        color_highlight = (1, 1, 1)
-
-        if axis == 'X':
-            color = axis_x
-        elif axis == 'Y':
-            color = axis_y
-        else:
-            color = axis_z
-
-        gzObject = GizmoInfo(scale_basis=1,
-                             color=color,
-                             color_highlight=color_highlight,
-                             use_draw_modal=False)
-
-        gz = gzObject.set_up(self, 'GIZMO_GT_arrow_3d')
-        prop = gz.target_set_operator("test.move_view_object", index=0)
-        prop.axis = axis
-
-        self._move_gz[gz] = axis
-
-    def correct_gz_loc(self, context):
-        for gz, axis in self._move_gz.items():
-            if axis == 'X':
-                rotate = Euler((math.radians(90), math.radians(
-                    180), math.radians(90)), 'XYZ')  # 奇怪的数值
-
-            elif axis == 'Y':
-                rotate = Euler((math.radians(-90), 0, 0), 'XYZ')
-
-            else:
-                rotate = Euler((0, 0, math.radians(90)), 'XYZ')
-
-            mx = context.object.matrix_world
-
-            # local
-            # rotate.rotate(mx.to_euler('XYZ'))
-            gz.matrix_basis = rotate.to_matrix().to_4x4()
-            gz.matrix_basis.translation = mx.translation
-
-    def refresh(self, context):
-        if context.object:
-            self.correct_gz_loc(context)
-
-
-class TEST_GGT_test_group3(GZGBase, bpy.types.GizmoGroup):
-    bl_label = "Test Widget"
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'WINDOW'
-    # 'TOOL_INIT' also sounds appropriate, but then the gizmo doesn't appear!
-    bl_options = {'3D'}
-
-    _move_gz = {}
-    mode = None
-
-    @classmethod
-    def poll(cls, context):
-        return context.object and len(context.selected_objects) > 0 and context.object.type == 'MESH'
-
-    def setup(self, context):
-        self.gravity_gz = None
-        self.mode = None
-        self._move_gz.clear()
-
-        self.update_gz_type(context)
-        self.correct_gz_loc(context)
-
-    def update_gz_type(self, context):
-        if self.mode != context.scene.dynamic_place_tool.mode:
-            self.mode = context.scene.dynamic_place_tool.mode
-
-            if context.scene.dynamic_place_tool.mode == 'FORCE':
-                if self.gravity_gz:
-                    self.gizmos.remove(self.gravity_gz)
-                    self.gravity_gz = None
-
-                self.add_move_gz(context, 'X')
-                self.add_move_gz(context, 'Y')
-                self.add_move_gz(context, 'Z')
-
-            elif context.scene.dynamic_place_tool.mode == 'GRAVITY':
-                for gz in self._move_gz.keys():
-                    self.gizmos.remove(gz)
-                self.add_gravity_gz(context)
-
-    def add_gravity_gz(self, context):
-        gzObject = GizmoInfo(scale_basis=1,
-                             use_draw_modal=False)
-
-        gz = gzObject.set_up(self, 'GIZMO_GT_arrow_3d')
-        prop = gz.target_set_operator("test.dynamic_place", index=0)
-        prop.axis = 'Z'
-
-        self.gravity_gz = gz
-
-    def add_move_gz(self, context, axis):
-        ui = bpy.context.preferences.themes[0].user_interface
-
-        axis_x = ui.axis_x[:3]
-        axis_y = ui.axis_y[:3]
-        axis_z = ui.axis_z[:3]
-        color_highlight = (1, 1, 1)
-
-        if axis == 'X':
-            color = axis_x
-        elif axis == 'Y':
-            color = axis_y
-        else:
-            color = axis_z
-
-        gzObject = GizmoInfo(scale_basis=1,
-                             color=color,
-                             color_highlight=color_highlight,
-                             use_draw_modal=False)
-
-        gz = gzObject.set_up(self, 'GIZMO_GT_arrow_3d')
-        gz.draw_style = 'BOX'
-        prop = gz.target_set_operator("test.dynamic_place", index=0)
-        prop.axis = axis
-
-        self._move_gz[gz] = axis
-
-    def correct_gz_loc(self, context):
-        try:
-            self.center = get_objs_bbox_center(
-                [obj for obj in context.selected_objects if obj.type == 'MESH'])
-
-        except ZeroDivisionError:
-            pass
-
-        for gz, axis in self._move_gz.items():
-            if axis == 'X':
-                rotate = Euler((math.radians(90), math.radians(
-                    180), math.radians(90)), 'XYZ')  # 奇怪的数值
-
-            elif axis == 'Y':
-                rotate = Euler((math.radians(-90), 0, 0), 'XYZ')
-
-            else:
-                rotate = Euler((0, 0, math.radians(90)), 'XYZ')
-
-            mx = context.object.matrix_world
-
-            # local
-            # rotate.rotate(mx.to_euler('XYZ'))
-            gz.matrix_basis = rotate.to_matrix().to_4x4()
-            gz.matrix_basis.translation = self.center
-
-        if self.gravity_gz:
-            rotate = Euler((math.radians(180), 0, 0), 'XYZ')
-            self.gravity_gz.matrix_basis = rotate.to_matrix().to_4x4()
-            self.gravity_gz.matrix_basis.translation = self.center
-
-    def refresh(self, context):
-        if context.object:
-            self.correct_gz_loc(context)
-            self.update_gz_type(context)
-
-
 classes = (
     PH_GZG_place_tool,
-    TEST_GGT_test_group2,
-    TEST_GGT_test_group3
+
 )
 
 
