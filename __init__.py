346d3c11f5311706b3310abe59eaef428c364228
atticus <1029910278@qq.com>
2023-02-16 11:34:30 +0800

    bump version to 1.1

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Place Helper 放置助手",
     "author": "AIGODLIKE社区(开发:Atticus)",
     "blender": (3, 2, 0),
-    "version": (1, 0),
+    "version": (1, 1),
     "category": "辣椒出品",
     "support": "COMMUNITY",
     "doc_url": "",
