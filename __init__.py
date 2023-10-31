cb521b3f54ad79cb9abdecf66fb261053cce8750
1029920178@qq.com <1029910278@qq.com>
2023-11-01 01:59:10 +0800

    update:更新版本到1.2.2

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Place Helper 放置助手",
     "author": "AIGODLIKE社区,Atticus",
     "blender": (3, 2, 0),
-    "version": (1, 2, 1),
+    "version": (1, 2, 2),
     "category": "辣椒出品",
     "support": "COMMUNITY",
     "doc_url": "",
