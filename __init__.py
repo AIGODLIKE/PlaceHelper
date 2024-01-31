9ac7dfb560f20a0913fd02a9dc2a4837fb0d9a3c
1029920178@qq.com <1029910278@qq.com>
2024-01-31 12:09:51 +0800

    bump 1.2.4

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Place Helper 放置助手",
     "author": "AIGODLIKE社区,Atticus",
     "blender": (4, 0, 0),
-    "version": (1, 2, 3),
+    "version": (1, 2, 4),
     "category": "辣椒出品",
     "support": "COMMUNITY",
     "doc_url": "",
