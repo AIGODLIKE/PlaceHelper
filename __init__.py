a310725604c27cf6a845348c6364b2d79923912a
小萌新 <3209970865@qq.com>
2025-02-06 17:51:02 +0800

    Feat: update version 1.2.8 -> 1.2.9 update author

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 bl_info = {
     "name": "Place Helper 放置助手",
-    "author": "AIGODLIKE社区,Atticus",
+    "author": "AIGODLIKE社区,Atticus,小萌新",
     "blender": (4, 1, 1),
-    "version": (1, 2, 8),
+    "version": (1, 2, 9),
     "category": "辣椒出品",
     "support": "COMMUNITY",
     "doc_url": "",
