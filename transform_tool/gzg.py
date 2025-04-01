5d2b10140aca45af6d0745c803fdb4a2f021ec0a
小萌新 <3209970865@qq.com>
2025-04-01 17:37:46 +0800

    Feat: gizmo alpha add pref prop

--- transform_tool/gzg.py
+++ transform_tool/gzg.py
@@ -145,7 +145,6 @@ class MoveGizmo:
             region = context.region
             region_3d = context.space_data.region_3d
             view_distance = context.space_data.region_3d.view_distance
-            # print("view_distance", view_distance)
 
             hide_distance = 8
             alpha_distance = 15
@@ -167,20 +166,13 @@ class MoveGizmo:
                 b = location_3d_to_region_2d(region, region_3d, axis_point)
                 distance = (a - b).length
 
-                # print(f"{axis}_offset =", gizmo.matrix_offset.__repr__())
-                # print("c=", a.__repr__(), "d=", b.__repr__(), )
-                # print(axis, distance, "a= ", origin_point.__repr__(), ";b=", axis_point.__repr__())
                 gizmo.hide = distance < hide_distance
                 if distance < alpha_distance:
                     factor = (distance - hide_distance) / hide_distance
                     gizmo.alpha = pref.gizmo_alpha * factor
-                    print("alpha move_plane_gizmos", axis, gizmo.hide, gizmo.alpha)
                 else:
                     gizmo.alpha = pref.gizmo_alpha
 
-            #
-            # min_distance = 5
-            # alpha_distance = 10
             for axis, gizmo in self.move_gizmos.items():
                 matrix = gizmo.matrix_basis
                 origin_point = matrix @ Vector()
@@ -196,9 +188,7 @@ class MoveGizmo:
                     gizmo.alpha = pref.gizmo_alpha * factor
                     axis_s = ["X", "Y", "Z"]
                     axis_s.remove(axis)
-                    print("distance move_gizmos", axis)
                     for ax in axis_s:
-                        print("sync panel ", axis, gizmo.hide)
                         pz = self.move_plane_gizmos[ax]
                         pz.hide = gizmo.hide
                         pz.alpha = gizmo.alpha
